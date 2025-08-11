828075cbe427ee690cc88e780c23d99a07a3b898
hjs0410hc <s7s7111@naver.com>
2025-08-11 17:27:29 +0900

    feat: 추가된 모든 페이지 경로 설정

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,11 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter, Route, Routes } from "react-router";
 import TestPage from "./pages/TestPage.jsx";
+import ChoiPage from "./pages/ChoiPage.jsx";
+import KimPage from "./pages/KimPage.jsx";
+import LeePage from "./pages/LeePage.jsx";
+import Oh from "./pages/Oh.jsx";
+import ParkPage from "./pages/ParkPage.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -11,6 +16,11 @@ createRoot(document.getElementById("root")).render(
       <Routes>
         <Route index element={<App />} />
         <Route path="/test" element={<TestPage />} />
+        <Route path="/choi" element={<ChoiPage />} />
+        <Route path="/kim" element={<KimPage />} />
+        <Route path="/lee" element={<LeePage />} />
+        <Route path="/oh" element={<Oh />} />
+        <Route path="/park" element={<ParkPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
