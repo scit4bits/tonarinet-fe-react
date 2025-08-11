61b55c493621d080b172ea216757a24309d3f282
jwoh0405 <moonlightgi@naver.com>
2025-08-11 16:21:25 +0900

    지원 작성

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <div>
+        <h1>Hello World</h1>
+      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
