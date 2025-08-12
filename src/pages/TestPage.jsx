419a006adc18baa8e9b835cba9e9b402bf84a248
hjs0410hc <s7s7111@naver.com>
2025-08-12 14:03:47 +0900

    feat: add ajax /w axios & event handler & useState, useEffect example code

--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -1,12 +1,80 @@
+import { useEffect, useState } from "react";
 import TestComponent from "../components/TestComponent";
+import axios from "axios";
 
 function TestPage() {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState("");
+  const [input, setInput] = useState("");
+  const [postResult, setPostResult] = useState(null);
+  useEffect(() => {
+    async function fetchData() {
+      const data = await axios.get("https://tnsv.thxx.xyz/api/simple/hello");
+
+      console.log(data.data);
+      setData(data.data);
+    }
+
+    fetchData();
+  }, []);
+
+  async function onClickHandlerOK() {
+    const payload = {
+      message: input,
+    };
+    const result = await axios.post(
+      "https://tnsv.thxx.xyz/api/simple/echo",
+      payload
+    );
+
+    setPostResult(result.data);
+    console.log(result);
+  }
+
+  async function onClickHandlerError() {
+    try {
+      const payload = {};
+      const result = await axios.post(
+        "https://tnsv.thxx.xyz/api/simple/echo",
+        payload
+      );
+      console.log(result);
+    } catch (e) {
+      setError(e.response.data.reply);
+      console.error(e);
+    }
+  }
+
+  function onInputHandler(event) {
+    setInput(event.target.value);
+  }
+
+  if (!data) {
+    return (
+      <>
+        <h1>Loading...</h1>
+      </>
+    );
+  }
+
+  if (error) {
+    return (
+      <>
+        <h1>{error}</h1>
+      </>
+    );
+  }
+
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <TestComponent />
-        </a>
+        <TestComponent />
+        <p>{data.reply}</p>
+
+        <input type="text" name="lel" onInput={onInputHandler} />
+        <button onClick={onClickHandlerError}>GO AJAX ERROR</button>
+        <button onClick={onClickHandlerOK}>GO AJAX OK</button>
+        {postResult && <p>{postResult.reply}</p>}
       </div>
     </>
   );
